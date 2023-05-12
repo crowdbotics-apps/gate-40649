3e193da0da595b84e51ba2f7be0f84978bbd2bfd
Crowdbotics <team@crowdbotics.com>
2023-05-12 13:57:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gate_40649/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gate_40649;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gate_40649";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gate-40649",
+  url: "https://gate-40649.botics.co",
+  api: "https://gate-40649.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
